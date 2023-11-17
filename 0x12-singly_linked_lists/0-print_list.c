de58103d06625cb244b23b96be357c82a723dc67
ziadhuss3d <ziadhuss3d@example.com>
2023-11-16 18:13:57 -0800

    print

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include "stdio.h"
+
+/**
+ * print_list - print the elements in list_t
+ * @z: pointer into head of list
+ *
+ * Return: nodes number
+ */
+size_t print_list(const list_t *z)
+{
+	size_t f = 0;
+
+	while (z)
+	{
+		if (z->str)
+			printf("[%u] %s\n", z->len, z->str);
+		else
+			printf("[0] (nill)\n");
+		z = z->next;
+		f++;
+	}
+	return (f);
+}
