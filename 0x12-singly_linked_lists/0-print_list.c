68ad2a198ba8d84c2373be0e90c13574e7734bbb
ziadhuss3d <ziadhuss3d@example.com>
2023-11-16 18:27:02 -0800

    nill --- nil

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,7 @@ size_t print_list(const list_t *z)
 		if (z->str)
 			printf("[%u] %s\n", z->len, z->str);
 		else
-			printf("[0] (nill)\n");
+			printf("[0] (nil)\n");
 		z = z->next;
 		f++;
 	}
