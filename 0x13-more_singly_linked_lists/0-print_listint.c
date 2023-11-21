c9152eef79fdb87448873bf9bc21f418624fa873
ziadhuss3d <ziadhuss3d@example.com>
2023-11-21 06:31:59 -0800

    ll

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,17 +2,20 @@
 
 /**
  * print_listint - print items for listint_t
- * @h: head list pointer
+ * @z: head list pointer
  * Return: node numbers
  */
-size_t print_listint(const listint_t *h)
+size_t print_listint(const listint_t *z)
 {
-	size_t cnt = 0;
+	size_t cnt = 1;
 
-	while (h)
+	if (z == NULL)
+		return (0);
+
+	while (z)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", z->n);
+		z = z->next;
 		cnt++;
 	}
 	return (cnt);
