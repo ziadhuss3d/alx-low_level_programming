98e8baa7e91d7eebe93c4b2b0642e1e1ed057908
ziadhuss3d <ziadhuss3d@example.com>
2024-05-07 10:25:48 -0700

    100

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,4 +7,6 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t size);
+size_t min(size_t a, size_t b);
+int jump_search(int *array, size_t size, int value);
 #endif
