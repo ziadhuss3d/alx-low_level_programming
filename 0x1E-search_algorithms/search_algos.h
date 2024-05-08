32f17024fbdb40bf82bf4a985cadc2a5a39f15cb
ziadhuss3d <ziadhuss3d@example.com>
2024-05-08 13:01:44 -0700

    100

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,4 +9,8 @@ int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t size);
 size_t min(size_t a, size_t b);
 int jump_search(int *array, size_t size, int value);
+int _advanced_binary(int *array, size_t left, size_t right, int value);
+int advanced_binary(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 #endif
